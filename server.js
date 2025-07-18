ae32b4500c536ff7a2151d0ac1660ed800fe8baa
firekidu <fcsfunfact@gmail.com>
2025-07-18 15:04:54 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,30 +1,24 @@
-// server.js
 import express from "express";
 import { createRequestHandler } from "@remix-run/express";
 import path from "path";
-import { fileURLToPath } from "url";
-import { dirname } from "path";
-import compression from "compression";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const BUILD_DIR = path.resolve("build");
 
-const port = process.env.PORT || 8788;
 const app = express();
 
-app.use(compression());
-app.use(express.static("public"));
+// Serve static files
+app.use("/build", express.static("public/build"));
 
+// Handle Remix requests
 app.all(
   "*",
   createRequestHandler({
     build: await import("./build/index.js"),
-    getLoadContext() {
-      return {};
-    },
+    mode: process.env.NODE_ENV,
   })
 );
 
+const port = process.env.PORT || 8788;
 app.listen(port, () => {
-  console.log(`✅ Remix app running at http://localhost:${port}`);
+  console.log(`✅ Express server is listening at http://localhost:${port}`);
 });
