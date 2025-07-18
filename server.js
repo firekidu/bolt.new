0917e27600bb4f4c6a1cc2c2b8e41674ac5a914e
firekidu <fcsfunfact@gmail.com>
2025-07-18 14:53:37 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
-const express = require('express');
-const { createRequestHandler } = require('@remix-run/express');
-const path = require('path');
+const express = require("express");
+const { createRequestHandler } = require("@remix-run/express");
+const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 8788;
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.all('*', createRequestHandler({ getLoadContext() {}, build: require('./build') }));
+app.all(
+  "*",
+  createRequestHandler({
+    build: require("./build"),
+    getLoadContext() {
+      return {};
+    },
+  })
+);
 
 app.listen(port, () => {
-  console.log(`✅ Remix app running on http://localhost:${port}`);
+  console.log(`✅ Remix app running at http://localhost:${port}`);
 });
